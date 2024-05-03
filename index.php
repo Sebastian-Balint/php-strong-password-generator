fdcb08aa721a2ad9947c14e035495e21ba894e8c
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-05-03 13:35:45 +0200

    add base

--- index.php
+++ index.php
@@ -1,2 +1,38 @@
 <?php
-echo "Hello World";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Strong Password Generator</title>
+</head>
+
+<body>
+
+    <div class="container my-5">
+        <h1>Strong Password Generator</h1>
+        <h4>Genera una pasword sicura</h4>
+    </div>
+
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-6">
+                <form action="index.php" method="POST">
+                    <label class="mb-3">Lunghezza password</label>
+                    <input class="form-control" type="number" name="numero" id="psw">
+                    <button class="btn btn-primary " type="submit">Invia</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
